59c50451330c97892d19ee689b940232e40eaecb
safinaa351 <safina.ayu.rasya.tik22@mhsw.pnj.ac.id>
2025-08-15 23:10:02 +0700

    add css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage User</title>
 
-    {{-- @vite(['resources/css/app.css']) --}}
+    @vite(['resources/css/app.css'])
 </head>
 <body>
     <h1>Welcome to Manage User Application</h1>
