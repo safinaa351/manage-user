2ba29d380e2e183b084181cb860ef96f0733f4d6
safinaa351 <safina.ayu.rasya.tik22@mhsw.pnj.ac.id>
2025-08-16 04:15:46 +0700

    add comment, fix json response clashing

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::get('/', function () {
 // Web Routes untuk interface
 Route::prefix('users')->name('users.')->group(function () {
     Route::get('/', [UserController::class, 'webIndex'])->name('index');
-    Route::get('/create', [UserController::class, 'create'])->name('create');
-    Route::post('/', [UserController::class, 'webStore'])->name('store');
-    Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [UserController::class, 'webUpdate'])->name('update');
+    Route::get('/create', [UserController::class, 'create'])->name('create'); //show the page where user can do create
+    Route::post('/', [UserController::class, 'webStore'])->name('store'); // do the create
+    Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit'); //show the page where user can do update
+    Route::put('/{id}', [UserController::class, 'webUpdate'])->name('update'); //do the update
     Route::delete('/{id}', [UserController::class, 'webDestroy'])->name('destroy');
 });
